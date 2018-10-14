47c0353fa60bdad63f865846993f8c972c87ff31
Ofen <614942+ofen@users.noreply.github.com>
2018-10-14 16:38:47 +0300

    minor css, js and template changes

--- public_html/static/js/custom.js
+++ public_html/static/js/custom.js
@@ -1,17 +1,3 @@
-$(':file').on('change', function() {
-    var filename = null;
-    if (this.files[0]) {
-        filename = this.files[0].name;
-    } else {
-        filename = '';
-    }
-    $(this).parent().siblings("[id^=file-info]").val(filename);
-});
-
-$('[id^=file-info]').on('click', function() {
-    $(this).siblings('.input-group-btn').click();
-});
-
 $('form').on('submit', function(e) {
     e.preventDefault();
     var form = $(this);
@@ -29,7 +15,7 @@ $('form').on('submit', function(e) {
 
             if (data.result == 'OK') {
                 form[0].reset();
-                $('#submitModal').modal({backdrop: 'static'});
+                $('#submitModal').modal();
             } else {
                 for (error in data.result) {
                     $(`#${error}`).parents('.form-group').addClass('has-error');
